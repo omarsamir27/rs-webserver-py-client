a43fbc6b012a2fb4bbd3548584b3e2b001d135a7
omar <omarmohameddrive@gmail.com>
2022-05-05 19:02:26 +0200

    handles requests and finds files

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,19 +1,25 @@
 extern crate core;
 
-use libwebs::http_magic;
-use libwebs::http_magic::{HttpHeaders, HttpRequest, HttpStatusCode, HttpVersion};
+use libwebs::{http_magic, utils};
+use libwebs::http_magic::{HttpHeaders, HttpMethod, HttpRequest, HttpResponse, HttpStatusCode, HttpVersion};
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::path::Path;
 use std::time::Duration;
-use std::{fs, io, net, thread};
+use std::{env, fs, io, net, thread};
+use std::fmt::format;
+use fsio::file;
+use fsio::path::as_path::AsPath;
+
+const SERVER_ROOT: &str = "srv/www" ;
+const BIND_ADDRESS:&str = "127.0.0.1:1025";
 
 fn setup() -> TcpListener {
-    let tcp_listener = net::TcpListener::bind("127.0.0.1:80").unwrap_or_else(|err| {
+    let tcp_listener = net::TcpListener::bind(BIND_ADDRESS).unwrap_or_else(|err| {
         println!("Could not start server");
         std::process::exit(-1);
     });
-    println!("Started server successfully on port 80");
+    println!("Started server successfully on {}",BIND_ADDRESS);
     tcp_listener
 }
 
@@ -27,7 +33,6 @@ fn setup() -> TcpListener {
 //     }
 //     clean_utf8
 // }
-
 fn read_stream(stream: &mut TcpStream) -> Vec<u8> {
     let mut buffer = vec![0u8; 1024];
     let mut data: Vec<u8> = Vec::with_capacity(1024);
@@ -46,7 +51,6 @@ fn read_stream(stream: &mut TcpStream) -> Vec<u8> {
     }
     data
 }
-
 
 fn process_stream(mut stream: TcpStream) {
     let mut request = HttpRequest::default();
@@ -67,32 +71,42 @@ fn process_stream(mut stream: TcpStream) {
             break;
         }
     }
-    let file = fs::read("/home/omar/srv/www/Screenshot.png").unwrap();
-    let mut hdr = HttpHeaders::new();
-    hdr.insert("Content-Type".to_string(), vec!["image/png".to_string()]);
-    let lol = http_magic::HttpResponse {
-        version: HttpVersion::HTTP1x0,
-        status: HttpStatusCode::Ok,
-        headers: hdr,
-        body: file,
-    };
-
-    stream.write_all(lol.to_vec().as_slice());
+    handle_request(&mut stream,&request);
 
 }
 
-fn handle_request(){
+fn handle_request(stream:&mut TcpStream,request:&HttpRequest){
+    if matches!(request.method,HttpMethod::GET) {
+        let mut path= request.requested_object.as_str()[1..].to_string();
+        if file::ensure_exists(path.as_str()).is_ok(){
+            let content = fs::read(path.as_str()).unwrap();
+            let response = HttpResponse{
+                version: request.version.clone(),
+                status: HttpStatusCode::Ok,
+                headers: HttpHeaders::from([
+                    ("Content-Type".to_string(),vec![utils::deduce_file_mime(path.as_path())]),
+                    ("Content-Length".to_string(),vec![content.len().to_string()])
+                ]),
+                body:content
+            };
+            stream.write_all(response.to_vec().as_slice());
+        }
+    }
+}
 
+fn path_setup(){
+    let mut www_root = format!("{}/{}",env::var("HOME").unwrap(),SERVER_ROOT);
+    println!("{}",www_root);
+    env::set_current_dir(www_root);
+    println!("{}",env::current_dir().unwrap().display());
 }
 
 fn main() {
+    path_setup();
     let listener = setup();
-    println!("{}",thread::available_parallelism().unwrap());
-    println!("{:?}",thread::current().id());
     let pool = rayon::ThreadPoolBuilder::new().num_threads(6).build().unwrap();
     for stream in listener.incoming() {
         pool.spawn(move || {
-            println!("{:?}",thread::current().id());
             process_stream(stream.unwrap());
         });
 
--- src/lib/utils.rs
+++ src/lib/utils.rs
@@ -1,4 +1,5 @@
 use std::fmt::Display;
+use std::path::Path;
 
 pub fn array_stringify<T: Display>(arr: &[T], delim: char) -> String {
     let mut string = String::new();
@@ -8,4 +9,14 @@ pub fn array_stringify<T: Display>(arr: &[T], delim: char) -> String {
     }
     string.pop();
     string
+}
+
+pub fn deduce_file_mime(path:&Path) -> String {
+    let guess = new_mime_guess::from_path(path);
+    if guess.first().is_some() {
+        return guess.first().unwrap().to_string()
+    }
+    else {
+        tree_magic::from_filepath(path)
+    }
 }
